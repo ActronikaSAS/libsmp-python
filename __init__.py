4c3721571ffe2c999f34f432b822148760bd16ad
Jérémy Cheynet <jeremy.cheynet@actronika.com>
2018-03-12 11:55:03 +0100

    chore: add `__init__.py` to create python module
    Signed-off-by: Jérémy Cheynet <jeremy.cheynet@actronika.com>

---
+++ __init__.py
@@ -0,0 +1,6 @@
+__all__ = ["frame", "smpValue", "smpMessage", "serialFrame"]
+
+from .frame import Frame
+from .smpValue import SmpType, SmpValue
+from .smpMessage import SmpMessage
+from .serialFrame import SerialFrame
